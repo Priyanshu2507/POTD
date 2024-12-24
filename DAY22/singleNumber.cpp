566f52ef82670c48df82e3c843c76f4e2d55c15d
Priyanshu2507 <priyanshushrivastava2507@gmail.com>
2024-12-24 17:59:24 +0530

    Second commit

---
+++ DAY22/singleNumber.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {4, 1, 2, 1, 2};
+    cout << solution.singleNumber(nums) << endl; 
+    return 0;
+}
--- Day21/string.cpp
+++ Day21/string.cpp
@@ -1,17 +1,27 @@
-class Solution {
-public:
-    int scoreOfString(string s) {
-        int a = 0 ,sum =0;
-        for(int i=0 ; i< s.length()-1;i++)
-        {
-            a=s[i+1] -s[i];
-            if(a<0)
-            {
-                a=a*(-1);
-            }
-            sum+=a;
-        }
-        return sum ;
-    }
+#include <bits/stdc++.h>
+using namespace std;
 
-};
+int scoreOfString(string s)
+{
+    int data = 0, sum = 0;
+    for (int i = 0; i < s.length() - 1; i++)
+    {
+        data = s[i + 1] - s[i];
+        if (data < 0)
+            data = data * (-1);
+        sum += data;
+    }
+    return sum;
+}
+
+int main()
+{
+    cout << "Enter your string: ";
+    string str;
+    cin >> str;
+    cout << "Score of string: " << scoreOfString(str) << endl;
+
+    return 0;
+}
+
+
